5628566a310b2e50b00536ba5881aeefa6d4cddd
mohit sharma <mohit.shar1005@gmail.com>
2018-09-03 17:04:38 +0530

    string interpolation

--- my-dream-app/src/app/server/server.component.ts
+++ my-dream-app/src/app/server/server.component.ts
@@ -4,4 +4,9 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
