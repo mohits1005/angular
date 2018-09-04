8a72ac0e2d13a3a702935a3ae0793b32265eade0
mohit sharma <mohit.shar1005@gmail.com>
2018-09-04 12:54:50 +0530

    using $event

--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -13,6 +13,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,5 +24,8 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+  onUpdateServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
