7f06f2b151b7383fc8a55d8bb82953aa640424a4
mohit sharma <mohit.shar1005@gmail.com>
2018-11-07 09:51:12 +0530

    string interpolation

--- learn-angular/src/app/server/server.component.ts
+++ learn-angular/src/app/server/server.component.ts
@@ -4,5 +4,9 @@ import {Component} from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-
+    serverId = 20;
+    serverStatus = 'offline';
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
