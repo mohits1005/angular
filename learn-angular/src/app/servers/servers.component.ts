9a99a64b6bcdd358b3a58f7246ff8cdae658804e
mohit sharma <mohit.shar1005@gmail.com>
2018-11-08 15:31:35 +0530

    using index in ngFor directive

--- learn-angular/src/app/servers/servers.component.ts
+++ learn-angular/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverName = 'Testserver';
   serverCreated = false;
   servers = ['Testserver', 'Testserver 2'];
+  log = [];
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -26,6 +27,7 @@ export class ServersComponent implements OnInit {
   onCreateServer(){
     this.serverCreated = true;
     this.servers.push(this.serverName);
+    this.log.push(new Date());
     this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
   }
   onUpdateServerName(event){
