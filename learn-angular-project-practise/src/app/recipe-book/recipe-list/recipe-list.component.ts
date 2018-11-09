fc52deea475b133c34a8ecaf7b79930832e10144
mohit sharma <mohit.shar1005@gmail.com>
2018-11-10 01:28:24 +0530

    Printing recipe, adding ingredient model

--- learn-angular-project-practise/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ learn-angular-project-practise/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg')
+  ];
 
   constructor() { }
 
