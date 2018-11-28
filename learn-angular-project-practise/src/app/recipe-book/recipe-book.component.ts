5ab898104fa64b829da34f6bbc5350b60a466723
mohit sharma <mohit.shar1005@gmail.com>
2018-11-29 01:23:00 +0530

    Passing data with event and property binding (combined)

--- learn-angular-project-practise/src/app/recipe-book/recipe-book.component.ts
+++ learn-angular-project-practise/src/app/recipe-book/recipe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-book.component.css']
 })
 export class RecipeBookComponent implements OnInit {
-
+  public recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  recipeLoad(recipeElement){
+    this.recipe = recipeElement.recipe;
   }
 
 }
--- learn-angular-project-practise/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ learn-angular-project-practise/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() loadRecipe = new EventEmitter<{ recipe: Recipe }>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  recipeClicked(recipe: Recipe){
+    this.loadRecipe.emit({ recipe });
   }
 
 }
