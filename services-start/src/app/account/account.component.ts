3c7d36fa16bbac5c66c634605061fbbb4ef3199c
mohit sharma <mohit.shar1005@gmail.com>
2018-12-01 00:50:36 +0530

    injecting service into components

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Account } from '../Account.model';
+import { LoggingService } from '../logging.service'
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input('accountElement') element: Account;
   @Output() changeStatus = new EventEmitter<{}>();
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
+
 
   ngOnInit() {
   }
@@ -17,7 +20,9 @@ export class AccountComponent implements OnInit {
   onStatusChange(id, status){
     // console.log('emit '+id+status);
     this.changeStatus.emit({ id: id, status: status });
-    console.log('A server status changed, new status: ' + status);
+    // console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
+
   }
 
 }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
-import { Account } from '../Account.model';
+import { LoggingService }  from '../logging.service'
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() addNewAccount = new EventEmitter<{}>();
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('statusInput') statusInputRef: ElementRef;
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit() {
   }
@@ -20,7 +21,9 @@ export class NewAccountComponent implements OnInit {
     const name = this.nameInputRef.nativeElement.value;
     const status = this.statusInputRef.nativeElement.value;
     this.addNewAccount.emit({name: name, status: status});
-    console.log('A server status changed, new status: ' + status);
+    // const service = new LoggingService();
+    // service.logStatusChange(status);
+    this.loggingService.logStatusChange(status);
   }
 
 }
