fe952ddecd6e6a1cabaa50f59712dd8bd5f33bac
mohit sharma <mohit.shar1005@gmail.com>
2018-12-05 15:07:45 +0530

    Using hostbinding in directives

--- learn-angular-project-practise/src/app/common/dropdown.directive.ts
+++ learn-angular-project-practise/src/app/common/dropdown.directive.ts
@@ -1,23 +1,12 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from "@angular/core";
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
   selector: "[appDropdown]"
 })
-export class DropdownDirective implements OnInit {
+export class DropdownDirective{
   state: boolean = false;
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
-
-  ngOnInit() {}
-    @HostListener('click', ['$event.target']) onClick(btn) {
-        if (this.state) {
-            //open
-            this.state = false;
-            this.renderer.removeClass(this.elRef.nativeElement, "open");
-        } else {
-            //close
-            this.state = true;
-            this.renderer.addClass(this.elRef.nativeElement, "open");
-        }
-    }
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') onClick() {
+      this.isOpen = !this.isOpen;
+  }
 }
