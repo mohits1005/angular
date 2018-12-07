49023200e1f6ed57cfdb6ff7bcd4752e841303b2
mohit sharma <mohit.shar1005@gmail.com>
2018-12-07 11:40:09 +0530

    adding shopping list form and validation

--- learn-angular-project-practise/src/app/app.module.ts
+++ learn-angular-project-practise/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -28,7 +29,7 @@ import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.compo
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
--- learn-angular-project-practise/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ learn-angular-project-practise/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
 import { Ingredient } from 'src/app/common/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  // @ViewChild("nameInput") nameInputRef: ElementRef;
+  // @ViewChild("amountInput") amountInputRef: ElementRef;
   @Output() addIngredient = new EventEmitter<{ newIngredient: Ingredient }>();
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onAddButton()
-  {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(name,amount);
+  onAddItem(form: NgForm) {
+    // const name = this.nameInputRef.nativeElement.value;
+    // const amount = this.amountInputRef.nativeElement.value;
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     // this.addIngredient.emit({ newIngredient });
     this.shoppingListService.addButtonClicked(newIngredient);
   }
-
 }
