aca8ea537c4a3e96698189bb56a63496f1855c40
mohit sharma <mohit.shar1005@gmail.com>
2018-12-11 01:37:54 +0530

    Recipe Edit form submit

--- learn-angular-project-practise/src/app/recipe-book/recipe.service.ts
+++ learn-angular-project-practise/src/app/recipe-book/recipe.service.ts
@@ -1,9 +1,10 @@
 import { EventEmitter} from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../common/ingredient.model';
+import { Subject } from 'rxjs';
 export class RecipeService{
     // loadRecipe = new EventEmitter<{ recipe: Recipe }>();
-
+    recipesChanged = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe('Spaghetti', 'Mouth watering Sphagetti!', 'https://gdsit.cdn-immedia.net/2017/01/CARNE-625x350.jpg',[new Ingredient('meat',1), new Ingredient('French Fries',20)]),
         new Recipe('Meat Balls', 'Anything tastier than a meatball?', 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/08/03/13/rio-food-picanha.jpg?w968h681',[new Ingredient('buns',2) , new Ingredient('meat',1)])
@@ -13,5 +14,15 @@ export class RecipeService{
     }
     getRecipe(index: number) {
         return this.recipes[index];
+    }
+    addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+        console.log(this.recipes.slice());
+    }
+    updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+        console.log(this.recipes.slice());
     }
 }
