f23e9d7c60637d659c45ba8a0a71bafce7d337dd
mohit sharma <mohit.shar1005@gmail.com>
2018-12-12 01:22:43 +0530

    form submit using ngForm and ngSubmit

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms-td-start';
+
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
+
+
 }
