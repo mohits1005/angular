9cab6744c38d95da12407c917f029a064e0c030f
mohit sharma <mohit.shar1005@gmail.com>
2018-12-13 01:12:59 +0530

    Syncing Reactive form using directives - formGroup and formControlName

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = "forms-reactive-start";
   genders = ["male", "female"];
   signupForm: FormGroup;
-  
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
 }
--- forms-reactive-start/src/app/app.module.ts
+++ forms-reactive-start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     ReactiveFormsModule
   ],
   providers: [],
