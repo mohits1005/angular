9687007ce019f937d7e769f609d5c0bbf3a961c1
mohit sharma <mohit.shar1005@gmail.com>
2018-12-13 10:19:50 +0530

    creating custom validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -8,22 +8,29 @@ import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 export class AppComponent implements OnInit {
   title = "forms-reactive-start";
   genders = ["male", "female"];
+  forbiddenUsernames = ["test"];
   signupForm: FormGroup;
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
       gender: new FormControl("male"),
       hobbies: new FormArray([])
     });
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);
   }
-  onAddHobby(){
+  onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+  forbiddenNames(control: FormControl):{[s:string]:boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value)){
+      return {'nameIsForbidden':true};
+    }
+    return null;
   }
 }
